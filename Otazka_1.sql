720cc78e3b29c7a5ac7f94f7b72449548545c1d6
Václav Cieslar <155641207+Vasek2301@users.noreply.github.com>
2025-02-17 21:37:52 +0100

    Otazka_1.sql

--- Otazka_1.sql
+++ Otazka_1.sql
@@ -1,11 +1,27 @@
 SELECT 
-    industry_name, 
-    MIN(year) AS first_year,
-    MAX(year) AS last_year,
-    MAX(CASE WHEN year = (SELECT MIN(year) FROM t_vaclav_cieslar_project_sql_primary_final) THEN average_wage END) AS first_wage,
-    MAX(CASE WHEN year = (SELECT MAX(year) FROM t_vaclav_cieslar_project_sql_primary_final) THEN average_wage END) AS last_wage,
-    (MAX(CASE WHEN year = (SELECT MAX(year) FROM t_vaclav_cieslar_project_sql_primary_final) THEN average_wage END) - 
-     MAX(CASE WHEN year = (SELECT MIN(year) FROM t_vaclav_cieslar_project_sql_primary_final) THEN average_wage END)) AS wage_change
-FROM t_vaclav_cieslar_project_sql_primary_final
-GROUP BY industry_name
+    p.industry_name,
+    MIN(p.year) AS first_year,
+    MAX(p.year) AS last_year,
+    (SELECT average_wage 
+     FROM t_vaclav_cieslar_project_sql_primary_final 
+     WHERE year = (SELECT MIN(year) FROM t_vaclav_cieslar_project_sql_primary_final)
+     AND industry_name = p.industry_name
+     LIMIT 1) AS first_wage,
+    (SELECT average_wage 
+     FROM t_vaclav_cieslar_project_sql_primary_final 
+     WHERE year = (SELECT MAX(year) FROM t_vaclav_cieslar_project_sql_primary_final)
+     AND industry_name = p.industry_name
+     LIMIT 1) AS last_wage,
+    ((SELECT average_wage 
+      FROM t_vaclav_cieslar_project_sql_primary_final 
+      WHERE year = (SELECT MAX(year) FROM t_vaclav_cieslar_project_sql_primary_final)
+      AND industry_name = p.industry_name
+      LIMIT 1) -
+     (SELECT average_wage 
+      FROM t_vaclav_cieslar_project_sql_primary_final 
+      WHERE year = (SELECT MIN(year) FROM t_vaclav_cieslar_project_sql_primary_final)
+      AND industry_name = p.industry_name
+      LIMIT 1)) AS wage_change
+FROM t_vaclav_cieslar_project_sql_primary_final p
+GROUP BY p.industry_name
 ORDER BY wage_change DESC;
